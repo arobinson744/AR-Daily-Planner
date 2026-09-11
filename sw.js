df159be634e1281755a9ceab8e274e214b632c5f
arobinson744 <adam.robinson20@hotmail.co.uk>
2026-09-11 15:28:45 +0100

    Update offline cache assets

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='ar-daily-planner-cloud-v1';
-const ASSETS=['./','./index.html','./manifest.webmanifest','./config.js','./icon-192.png','./icon-512.png'];
+const CACHE='ar-daily-planner-cloud-v2';
+const ASSETS=['./','./index.html','./manifest.webmanifest','./config.js','./icon.svg'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
 self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
